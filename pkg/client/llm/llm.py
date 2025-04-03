e13e3178078657aebeb8e207ca5aedd81a8c5174
xiaoy <1103098607@qq.com>
2025-04-03 15:30:30 +0800

    feat: 实现 ai agent client；

--- pkg/client/llm/llm.py
+++ pkg/client/llm/llm.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain_core.tools import Tool, StructuredTool
 from langchain_openai import ChatOpenAI
-from langchain_core.tools import Tool
 from langchain_core.prompts import ChatPromptTemplate
 from pydantic import BaseModel, Field
 
@@ -10,58 +10,101 @@ from pkg.util.config.config import config_manager
 
 
 class ModelEnum(str, Enum):
+    gpt4o_mini = "gpt-4o-mini"
+
     qwen_turbo = "qwen-turbo"
     qwen_plus = "qwen-plus"
 
 
 
-# 定义工具（使用 Tool 类显式定义）
-def magic_function(input: int) -> int:
-    """Applies a magic function to an input.
-    input : int
-    """
-    return input + 2
+class BaseAIAgent:
+    def __init__(self):
+        self.llm = None
+        self.agent_executor = None
+        self.agent = None
+        self.model = None
+        self.tools = []
+        self.prompt_template = None
+        self.init()
 
-class MagicFunctionInput(BaseModel):
-    input: int = Field(..., description="输入整数")
+    def init(self):
+        self.init_llm(
+            api_key=config_manager.get_config().llm.api_key,
+            base_url="https://api.openai.com/v1",
+            model=ModelEnum.gpt4o_mini.value
+        )
+        self.init_tools()
+        self.init_prompt_template()
+        self.init_agent()
+        self.init_agent_executor()
+
+    def init_llm(self, api_key, base_url, model):
+        self.llm = ChatOpenAI(
+            api_key=api_key,
+            base_url=base_url,
+            model=model,
+        )
+
+    def init_agent(self):
+        self.agent = create_tool_calling_agent(
+            llm=self.llm,
+            tools=self.tools,
+            prompt=self.prompt_template,
+        )
+
+    def init_tools(self):
+        self.tools = []
+
+    def init_prompt_template(self):
+        self.prompt_template = ChatPromptTemplate.from_messages(
+            [
+                ("system", "你是一个会调用工具的智能助手，按需执行任务。"),
+                ("human", "{input}"),
+                ("placeholder", "{agent_scratchpad}")
+            ]
+        )
+
+    def init_agent_executor(self):
+        self.agent_executor = AgentExecutor(
+            agent=self.agent,
+            tools=self.tools,
+            verbose=True
+        )
 
 
-tools = [
-    Tool(
-        name="magic_function",
-        func=magic_function,
-        description="对输入整数执行魔法函数（+2）, input: int",
-        args_schema=MagicFunctionInput,
-        return_direct=False
-    )
-]
+class QwenTurboAgent(BaseAIAgent):
+    def init(self):
+        self.init_llm(
+            api_key=config_manager.get_config().llm.api_key,
+            base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+            model=ModelEnum.qwen_turbo.value
+        )
+        self.init_prompt_template()
+        self.init_tools()
+        self.init_agent()
+        self.init_agent_executor()
 
-chatLLM = ChatOpenAI(
-    api_key=config_manager.get_config().llm.api_key,
-    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-    model=ModelEnum.qwen_turbo.value,
-)
+    def init_tools(self):
+        def magic_function(input: int) -> int:
+            """Applies a magic function to an input.
+            input : int
+            """
+            return input + 2
 
-# 定义提示模板（结构需符合 LangChain 0.3 的要求）
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "你是一个会调用工具的智能助手，按需执行任务。"),
-        ("human", "{input}"),
-        ("placeholder", "{agent_scratchpad}")  # 用于记录代理的中间思考
-    ]
-)
+        class MagicFunctionInput(BaseModel):
+            input: int = Field(..., description="输入整数")
 
-# 创建 Tool Calling Agent
-agent = create_tool_calling_agent(
-    llm=chatLLM,
-    tools=tools,
-    prompt=prompt,
-)
+        s = StructuredTool.from_function(
+                func=magic_function,
+                name="magic_function",
+                description="对输入整数执行魔法函数（+2）, input: int",
+                args_schema=MagicFunctionInput,
+                return_direct=False
+            )
 
-# 初始化 AgentExecutor
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True
-)
+        self.tools = [
+            s
+        ]
 
+    def invoke(self, query):
+        return self.agent_executor.invoke(query)
--- pkg/client/llm/test_llm.py
+++ pkg/client/llm/test_llm.py
@@ -1,20 +1,10 @@
-from pkg.client.llm.llm import agent_executor
-
+from pkg.client.llm.llm import QwenTurboAgent
 
 def test_llm():
     # 执行工具调用
-    result = agent_executor.invoke(
+    agent = QwenTurboAgent()
+
+    result = agent.invoke(
         {"input": "请计算 magic_function(3) 的结果"}
     )
     assert result["output"] == 5 or "5" in result["output"]
-
-    # # 测试带聊天历史的场景
-    # from langchain_core.messages import HumanMessage, AIMessage
-    # result_with_history = agent_executor.invoke({
-    #     "input": "我的名字是什么？",
-    #     "chat_history": [
-    #         HumanMessage(content="你好，我的名字是 Alice"),
-    #         AIMessage(content="你好 Alice！有什么可以帮助你的吗？")
-    #     ]
-    # })
-    # assert "Alice" in str(result_with_history["output"])
